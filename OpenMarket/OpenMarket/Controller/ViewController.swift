a6174ecbc0f85af0d1f512ebb50aa4045e31313f
cheshire.kwon <cheshire.kwon@kakaocorp.com>
2022-01-26 23:19:44 +0900

    feat: collection view로 리스트 만들기.....

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -6,33 +6,102 @@
 
 import UIKit
 
+private enum Section: Hashable {
+    case main
+}
+
 class ViewController: UIViewController {
     
-    var testProductList: ProductList? = nil
+    // api data
     let networkController = NetworkConnector()
+    var testProductList: ProductList? = nil
+    var products: [Product] = []
     
+    // storyboard
     @IBOutlet weak var segmentControl: UISegmentedControl!
-    
     @IBOutlet weak var testLabel: UILabel!
+    
+    // custom cell list view
+    private var collectionView: UICollectionView! = nil
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Product>! = nil
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchProductListData(page: 1, num: 5)
-        setSegmentControl()
+        fetchProductListData(page: 1, num: 20)
+//        setSegmentControl()
     }
-    
+}
+
+// for fetch data
+extension ViewController {
     func fetchProductListData(page pageNumber: Int, num itemsPerPage: Int) {
         self.networkController.requestGET(path: "api/products?page_no=\(pageNumber)&items_per_page=\(itemsPerPage)", type: ProductList.self) {
             result in
             switch result {
             case .success(let data):
                 self.testProductList = data
+                self.products = data.products
+                self.configureCollectionView()
+                self.configureDataSource()
             case .failure(let error):
                 print(error.description)
             }
         }
     }
+}
+
+// custom cell list view
+extension ViewController {
+    private func createLayout() -> UICollectionViewLayout {
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
     
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<ProductListCell, Product> { (cell, indexPath, item) in
+            cell.update(with: item)
+            cell.accessories = [.disclosureIndicator()]
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Product>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: Product) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
+        }
+        
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Product>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(products)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+
+
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
+
+
+
+
+
+
+
+
+extension ViewController {
+    // for segment controller
     func setSegmentControl() {
         segmentControl.selectedSegmentIndex = 0
         segmentControl.addTarget(self, action: #selector(segmentControlchanged), for: UIControl.Event.valueChanged)
@@ -50,9 +119,6 @@ class ViewController: UIViewController {
         }
     }
 }
-
-
-
 
 
 
