cf953883e7f8fb8ff25b8f61d6fde2e8e3b874e9
cheshire.kwon <cheshire.kwon@kakaocorp.com>
2022-01-26 18:04:42 +0900

    feat: segment 추가

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -8,65 +8,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var myTableView: UITableView!
-    
     var testProductList: ProductList? = nil
     let networkController = NetworkConnector()
     
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        myTableView.delegate = self
-        myTableView.dataSource = self
-        
-        networkController.checkHealth() {
+        fetchProductListData(page: 1, num: 5)
+        setSegmentControl()
+    }
+    
+    func fetchProductListData(page pageNumber: Int, num itemsPerPage: Int) {
+        self.networkController.requestGET(path: "api/products?page_no=\(pageNumber)&items_per_page=\(itemsPerPage)", type: ProductList.self) {
             result in
             switch result {
             case .success(let data):
-                if data == true {
-                    let pageNumber = 1
-                    let itemsPerPage = 20
-                    self.networkController.requestGET(path: "api/products?page_no=\(pageNumber)&items_per_page=\(itemsPerPage)", type: ProductList.self) {
-                        result in
-                        switch result {
-                        case .success(let data):
-                            self.testProductList = data
-                            self.myTableView.reloadData()
-                        case .failure(let error):
-                            print(error.description)
-                        }
-                    }
-                    let productId = 522
-                    self.networkController.requestGET(path: "api/products/\(productId)", type: ProductDetail.self) {
-                        result in
-                        switch result {
-                        case .success(let data):
-                            print(data)
-                        case .failure(let error):
-                            print(error.description)
-                        }
-                    }
-                } else {
-                    print("check state of the api")
-                }
+                self.testProductList = data
             case .failure(let error):
                 print(error.description)
             }
-        } 
+        }
+    }
+    
+    func setSegmentControl() {
+        segmentControl.selectedSegmentIndex = 0
+        segmentControl.addTarget(self, action: #selector(segmentControlchanged), for: UIControl.Event.valueChanged)
+    }
+    
+    @objc
+    func segmentControlchanged() {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            testLabel.text = "list!"
+        case 1:
+            testLabel.text = "grid!"
+        default:
+            testLabel.text = "sth wrong"
+        }
     }
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testProductList?.products.count ?? 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: TmpTableViewCell = myTableView.dequeueReusableCell(withIdentifier: "TmpTableViewCell", for: indexPath) as! TmpTableViewCell
-        cell.myLabel.text = testProductList?.products[indexPath.row].name
-        return cell
-    }
-}
+
+
+
 
 
 
